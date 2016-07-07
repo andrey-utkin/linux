3a7323f14065f3c4b3ff7e69479b6a6003eb320a
Andrey Utkin <andrey_utkin@fastmail.com>
2016-07-07 23:17:57 +0300

    led: is31fl319x: Rework DT parsing, fix static analysis warnings, etc

--- drivers/leds/leds-is31fl319x.c
+++ drivers/leds/leds-is31fl319x.c
@@ -68,14 +68,24 @@
 
 #define NUM_LEDS 9	/* max for 3199 chip */
 
+#define LED_MAX_MICROAMP_UPPER_LIMIT ((u32) 40000)
+#define LED_MAX_MICROAMP_LOWER_LIMIT ((u32) 5000)
+#define LED_MAX_MICROAMP_DEFAULT ((u32) 20000)
+#define LED_MAX_MICROAMP_STEP ((u32) 5000)
+
+#define AUDIO_GAIN_DB_MAX ((u32) 21)
+
 struct is31fl319x_chip {
 	struct i2c_client	*client;
 	struct regmap		*regmap;
 	struct mutex            lock;
+	u32                     audio_gain_db;
 
 	struct is31fl319x_led {
 		struct is31fl319x_chip	*chip;
 		struct led_classdev	led_cdev;
+		u32                     max_microamp;
+		bool                    configured;
 	} leds[NUM_LEDS];
 };
 
@@ -97,19 +107,17 @@ static int is31fl319x_brightness_set(struct led_classdev *led_cdev,
 						  struct is31fl319x_led,
 						  led_cdev);
 	struct is31fl319x_chip *is31 = led->chip;
+	int chan = led - is31->leds;
 	int ret;
-
 	int i;
 	u8 ctrl1 = 0, ctrl2 = 0;
 
-	dev_dbg(&is31->client->dev, "%s %ld: %d\n", __func__, led - is31->leds,
-		brightness);
+	dev_dbg(&is31->client->dev, "%s %d: %d\n", __func__, chan, brightness);
 
 	mutex_lock(&is31->lock);
 
 	/* update PWM register */
-	ret = regmap_write(is31->regmap, IS31FL319X_PWM1 + (led - is31->leds),
-			   brightness);
+	ret = regmap_write(is31->regmap, IS31FL319X_PWM1 + chan, brightness);
 	if (ret < 0)
 		goto out;
 
@@ -158,54 +166,103 @@ out:
 	return ret;
 }
 
-static struct led_info *
-is31fl319x_parse_dt(struct i2c_client *client, int num_leds)
+static int is31fl319x_parse_child_dt(const struct device *dev,
+				     const struct device_node *child,
+				     struct is31fl319x_led *led)
 {
-	struct device_node *np = client->dev.of_node, *child;
-	struct led_info *is31_leds;
-	int count;
+	struct led_classdev *cdev = &led->led_cdev;
+	int ret;
 
-	if (!np)
-		return ERR_PTR(-ENODEV);
+	cdev->name = NULL;
+	ret = of_property_read_string(child, "label", &cdev->name);
+	if (ret < 0 && ret != -EINVAL)	/* is optional */
+		return ret;
 
-	count = of_get_child_count(np);
-	dev_dbg(&client->dev, "child count %d\n", count);
-	if (!count || count > NUM_LEDS)
-		return ERR_PTR(-ENODEV);
+	cdev->default_trigger = NULL;
+	ret = of_property_read_string(child, "linux,default-trigger",
+		&cdev->default_trigger);
+	if (ret < 0 && ret != -EINVAL)	/* is optional */
+		return ret;
 
-	is31_leds = devm_kzalloc(&client->dev,
-			sizeof(struct led_info) * NUM_LEDS, GFP_KERNEL);
-	if (!is31_leds)
-		return ERR_PTR(-ENOMEM);
-
-	for_each_child_of_node(np, child) {
-		struct led_info led;
-		u32 reg;
-		int ret;
-
-		led.name = NULL;
-		ret = of_property_read_string(child, "label", &led.name);
-		if (ret < 0 && ret != -EINVAL)	/* is optional */
-			return ERR_PTR(ret);
-		led.default_trigger = NULL;
-		ret = of_property_read_string(child, "linux,default-trigger",
-			&led.default_trigger);
-		if (ret < 0 && ret != -EINVAL)	/* is optional */
-			return ERR_PTR(ret);
-		led.flags = 0;
-		ret = of_property_read_u32(child, "reg", &reg);
-		dev_dbg(&client->dev, "name = %s reg = %d\n", led.name, reg);
-		reg -= 1;	/* index 0 is at reg = 1 */
-		if (ret != 0 || reg < 0 || reg >= num_leds)
-			continue;
-
-		if (is31_leds[reg].name)
-			dev_err(&client->dev, "duplicate led line %d for %s -> %s\n",
-				reg, is31_leds[reg].name, led.name);
-		is31_leds[reg] = led;
+	led->max_microamp = LED_MAX_MICROAMP_DEFAULT;
+	ret = of_property_read_u32(child, "led-max-microamp",
+				   &led->max_microamp);
+	if (!ret) {
+		led->max_microamp = clamp(led->max_microamp,
+					  LED_MAX_MICROAMP_LOWER_LIMIT,
+					  LED_MAX_MICROAMP_UPPER_LIMIT);
+		led->max_microamp -= led->max_microamp % LED_MAX_MICROAMP_STEP;
 	}
 
-	return is31_leds;
+	return 0;
+}
+
+static int is31fl319x_parse_dt(struct device *dev,
+			       struct is31fl319x_chip *is31)
+{
+	struct device_node *np = dev->of_node, *child;
+	struct led_info *is31_leds;
+	int count;
+	int ret;
+
+	if (!np)
+		return -ENODEV;
+
+	count = of_get_child_count(np);
+
+	dev_dbg(dev, "child count %d\n", count);
+	if (!count || count > NUM_LEDS)
+		return -ENODEV;
+
+	is31_leds = devm_kzalloc(dev, sizeof(struct led_info) * NUM_LEDS,
+				 GFP_KERNEL);
+	if (!is31_leds)
+		return -ENOMEM;
+
+	for_each_child_of_node(np, child) {
+		struct is31fl319x_led *led;
+		u32 reg;
+
+		ret = of_property_read_u32(child, "reg", &reg);
+		if (ret)
+			break;
+
+		if (reg < 1 || reg > NUM_LEDS) {
+			dev_err(dev, "invalid led reg %u\n", reg);
+			ret = -EINVAL;
+			break;
+		}
+
+		led = &is31->leds[reg - 1];
+
+		if (led->configured) {
+			dev_err(dev, "led %u is already configured\n", reg);
+			ret = -EINVAL;
+			break;
+		}
+
+		ret = is31fl319x_parse_child_dt(dev, child, led);
+		if (ret) {
+			dev_err(dev, "led %u DT parsing failed\n", reg);
+			break;
+		}
+
+		led->configured = true;
+	}
+	if (ret) {
+		dev_err(dev, "DT child nodes parsing failed, error %d\n", ret);
+		return ret;
+	}
+
+	is31->audio_gain_db = 0;
+	ret = of_property_read_u32(np, "audio-gain-db", &is31->audio_gain_db);
+	if (!ret) {
+		is31->audio_gain_db = min(is31->audio_gain_db,
+					  AUDIO_GAIN_DB_MAX);
+		is31->audio_gain_db -= is31->audio_gain_db % 3;
+	}
+
+	return 0;
 }
 
 static const struct of_device_id of_is31fl319x_leds_match[] = {
@@ -236,7 +293,7 @@ static bool is31fl319x_volatile_reg(struct device *dev, unsigned int reg)
 	}
 }
 
-struct regmap_config regmap_config = {
+static struct regmap_config regmap_config = {
 	.reg_bits = 8,
 	.val_bits = 8,
 	.max_register = IS31FL319X_REG_CNT,
@@ -250,9 +307,10 @@ static int is31fl319x_probe(struct i2c_client *client,
 {
 	struct is31fl319x_chip *is31;
 	struct i2c_adapter *adapter;
-	struct led_info *leds;
 	int err;
 	int i = 0;
+	u8 config2 = 0;
+	u32 aggregated_led_microamp = LED_MAX_MICROAMP_UPPER_LIMIT;
 
 	adapter = to_i2c_adapter(client->dev.parent);
 
@@ -262,18 +320,17 @@ static int is31fl319x_probe(struct i2c_client *client,
 	if (!i2c_check_functionality(adapter, I2C_FUNC_I2C))
 		return -EIO;
 
-	leds = is31fl319x_parse_dt(client, (int) id->driver_data);
-	if (IS_ERR(leds)) {
-		dev_err(&client->dev, "DT parsing error %d\n",
-			(int) PTR_ERR(leds));
-		return PTR_ERR(leds);
-	}
-
 	is31 = devm_kzalloc(&client->dev, sizeof(*is31), GFP_KERNEL);
 	if (!is31)
 		return -ENOMEM;
 
 	mutex_init(&is31->lock);
+
+	err = is31fl319x_parse_dt(&client->dev, is31);
+	if (err) {
+		dev_err(&client->dev, "DT parsing error %d\n", err);
+		return err;
+	}
 
 	is31->client = client;
 	is31->regmap = devm_regmap_init_i2c(client, &regmap_config);
@@ -298,42 +355,34 @@ static int is31fl319x_probe(struct i2c_client *client,
 	for (i = 0; i < NUM_LEDS; i++)
 		regmap_write(is31->regmap, IS31FL319X_PWM1 + i, 0x00);
 
-	if (client->dev.of_node) {
-		u32 val;
-		u8 config2 = 0;
+	/*
+	 * Kernel conventions require per-LED led-max-microamp property.
+	 * But the chip does not allow to limit individual LEDs.
+	 * So we take minimum from all subnodes.
+	 */
+	for (i = 0; i < NUM_LEDS; i++)
+		if (is31->leds[i].configured &&
+		    is31->leds[i].max_microamp < aggregated_led_microamp)
+			aggregated_led_microamp = is31->leds[i].max_microamp;
 
-		if (!of_property_read_u32(client->dev.of_node,
-					 "led-max-microamp", &val)) {
-			if (val > 40000)
-				val = 40000;
-			if (val < 5000)
-				val = 5000;
-			config2 |= (((64000 - val) / 5000) & 0x7) << 4; /* CS */
-		}
-		if (!of_property_read_u32(client->dev.of_node, "audio-gain-db",
-					 &val)) {
-			if (val > 21)
-				val = 21;
-			config2 |= val / 3; /* AGS */
-		}
-		regmap_write(is31->regmap, IS31FL319X_CONFIG2, config2);
-	}
+	/* CS */
+	/* TODO FIXME calculation seems wrong */
+	config2 |= (((64000 - aggregated_led_microamp) / 5000) & 0x7) << 4;
+	config2 |= is31->audio_gain_db / 3; /* AGS */
+	regmap_write(is31->regmap, IS31FL319X_CONFIG2, config2);
 
 	for (i = 0; i < NUM_LEDS; i++) {
-		struct is31fl319x_led *l = is31->leds + i;
+		struct is31fl319x_led *l = &is31->leds[i];
+
+		if (!l->configured)
+			continue;
 
 		l->chip = is31;
-		if (leds[i].name && !leds[i].flags) {
-			l->led_cdev.name = leds[i].name;
-			l->led_cdev.default_trigger
-				= leds[i].default_trigger;
-			l->led_cdev.brightness_set_blocking
-				= is31fl319x_brightness_set;
-			err = devm_led_classdev_register(&client->dev,
-						    &l->led_cdev);
-			if (err < 0)
-				return err;
-		}
+		l->led_cdev.brightness_set_blocking = is31fl319x_brightness_set;
+
+		err = devm_led_classdev_register(&client->dev, &l->led_cdev);
+		if (err < 0)
+			return err;
 	}
 
 	return 0;
